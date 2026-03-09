ee10934f3d3c213e6cdf1db24de490d6f359216f
zhaoyubin-core <2316457942@qq.com>
2026-03-09 19:49:59 +0800

    Revert to standard Vercel Next.js config

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  // Added comment to trigger redeploy
+  /* config options here */
 };
 
 export default nextConfig;
